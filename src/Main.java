595d9651cc7cfa7c7c5a8f1d998787b0f2d37fab
i-suon <47300337+i-suon@users.noreply.github.com>
2019-02-03 19:26:20 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,10 @@ public class Main {
         ArrayList<Item> itemList = simulation.loadItems("Phase-1.txt");
         ArrayList<Item> itemList2 = simulation.loadItems("Phase-2.txt");
 
-
-/*        for (int i = 0; i < itemList.size(); i++) {
-            Item item = itemList.get(i);
-            System.out.println(item.name + " " + item.weight);
-        }
-
-        for (int i = 0; i < itemList2.size(); i++) {
-            Item item = itemList2.get(i);
-            System.out.println(item.name + " " + item.weight);
-        }
-*/
-
         ArrayList<Rocket> rocketList = simulation.loadU1(itemList);
         ArrayList<Rocket> rocketList2 = simulation.loadU2(itemList);
         ArrayList<Rocket> rocketList3 = simulation.loadU1(itemList2);
         ArrayList<Rocket> rocketList4 = simulation.loadU2(itemList2);
-
-/*      for (Rocket i : rocketList) {
-            System.out.println("Rocket " + (i+1) + ":\n" + rocketList.get(i).currentWeight);
-        }
-        System.out.println("\n");
-        for (int i = 0; i < rocketList2.size(); i++) {
-            System.out.println("Rocket " + (i+1) + ":\n" + rocketList2.get(i).currentWeight);
-        }
-*/
 
         int fullBudgetU1 = simulation.runSimulation(rocketList) + simulation.runSimulation(rocketList3);
         int fullBudgetU2 = simulation.runSimulation(rocketList2) + simulation.runSimulation(rocketList4);
